b6d66958f38e3a810bc984483deb6bf12a408458
ccatina <hungaries7@gmail.com>
2019-08-08 21:46:19 +0300

    Add check, need validator and generathion new file, hasn't check memory

--- sortbigdouble/sortdouble.cpp
+++ sortbigdouble/sortdouble.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int countstrings()
@@ -7,43 +8,59 @@ int countstrings()
 	int i = 0;
 	if (in.is_open())
 	{
-		while(!in.eof)
+		while (!in.eof())
 			i++;
 	}
 	return (i);
 }
 
-string searchmin()
+bool checksort(std::string str)
 {
-	int n = 0;
-	std::ofstream out("sorted.txt", std::ios::app);
+	std::string temp;
+	std::ifstream out("sorted.txt");
+
+	while (getline(out, temp))
+	{
+		if (str == temp)
+		{
+			out.close();
+			return (true);
+		}
+	}
+	out.close();
+	return (false);
+}
+
+void sort()
+{
+	std::string str1;
+	std::string str2;	
+	
 	std::ifstream in("unsorted.txt");
+	
 	if (in.is_open())
 	{
-		while 
+		for (int k = 0; k < countstrings(); k++)
+		{
+			int n = 1;
+			getline(in, str1);
+			while (getline(in, str2))
+			{	
+				if (str1.compare(str2) == 1 && checksort(str2) == false)
+					str1 = str2;
+				else if (str1.compare(str2) == 0)
+					n++;
+			}
+			std::ofstream out("sorted.txt", std::ios::app);	
+			for (int l = 0; l < n; l++)
+				out << str1 << "\n";
+			out.close();
+			in.seekg(0, std::ios::beg);
+		}
 	}
+	in.close();
 }
 
 int main()
 {
-	std::string str1;
-	std::string str2;
-
-	std::ifstream in("unsorted.txt");
-	std::ofstream out("sorted.txt", std::ios::app);
-	int i = 1;
-	if (in.is_open())
-	{
-		getline(in, str1);
-		while (getline(in, str2))
-		{
-		       if (compare(str1, str2) == 1)
-		       {
-			       str1 = str2;
-			       i++;
-		       }
-		}
-		in.seekg(0, std::ios::begin);
-	}
-	in.close();
 }
